c0597b7d36a3a5909665e70456918604a848066e
gabess3 <95196142+gabess3@users.noreply.github.com>
2023-03-06 22:28:12 -0600

    updated controllers to handle update requests.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -47,8 +47,26 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(
+      {
+      category_name: req.body.category_name
+      },
+      {
+        where: { 
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json(updatedCategory);
+  }
+  catch { (err) => {
+    console.log(err)
+    res.status(500);
+    }
+  }
 });
 
 router.delete('/:id', async (req, res) => {
